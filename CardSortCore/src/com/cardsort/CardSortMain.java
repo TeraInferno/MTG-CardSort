a1075a4630566de92022135783c234ecca96eefb
Andrew Grossnickle <TeraInferno@users.noreply.github.com>
2016-09-18 19:24:18 -0600

    Added initial files

---
+++ CardSortCore/src/com/cardsort/CardSortMain.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.cardsort;
+
+
+/**
+ * @author agrossnickle
+ *
+ */
+public class CardSortMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
---
+++ CardSortCore/src/com/cardsort/util/ApiUtil.java
@@ -0,0 +1,25 @@
+package com.cardsort.util;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.apache.ApacheHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+
+public class ApiUtil {
+	
+	
+	  static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+	  static final JsonFactory JSON_FACTORY = new GsonFactory();
+	
+	public void fetchCardInfo(String card){
+		
+		
+		
+	}
+
+
+
+
+
+}
